9e17ae99d2bcc5de9f649a4c12211320a56195f1
kjha4951 <kjha4951@gmail.com>
2024-08-24 07:10:44 +0530

    db

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/book',bookroute);
 
-const PORT = process.env.PORT||5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT||5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
